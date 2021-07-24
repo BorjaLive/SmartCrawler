14a765d11d56f51d6d4c768d32598ce424a74122
BorjaLive <borjainlive@gmail.com>
2021-07-24 16:54:24 +0200

    Eliminar claves

--- web/keys.php
+++ web/keys.php
@@ -1,8 +1,8 @@
 <?php
-Const SETTINGS = [
-	'consumer_key' => "PEnThSicnJwkXc6xH0swsug0h", 
-	'consumer_secret' => "ba8GDbu5MeGDbetNiThGPNxE3uEKpqJ27CA0Lqp58e6jvpZA9g",
-	'oauth_access_token' => "1491086923-CkO8FfvWoIDdtI6BjVkLvrEIdEHmdtVkbRSP6jd",
-	'oauth_access_token_secret' => "RM3A8ChwrEP5QgzVkRBwEZXMfEZGOKZJGQaFLct5ncSzZ"
-];
+	Const SETTINGS = [
+		'consumer_key' => "PEnThSicnJwkXc6xH0swsug0h", 
+		'consumer_secret' => "ba8GDbu5MeGDbetNiThGPNxE3uEKpqJ27CA0Lqp58e6jvpZA9g",
+		'oauth_access_token' => "1491086923-CkO8FfvWoIDdtI6BjVkLvrEIdEHmdtVkbRSP6jd",
+		'oauth_access_token_secret' => "RM3A8ChwrEP5QgzVkRBwEZXMfEZGOKZJGQaFLct5ncSzZ"
+	];
 ?>
